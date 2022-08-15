5158cb94109ee935c4bb73141e94026504ad38fd
Juana Ng <juanang@Juanas-MacBook-Pro.local>
2022-08-15 17:01:22 -0400

    added side nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,20 +2,62 @@ import React, { Component } from "react";
 import { ReactComponent as Search } from "../../images/search-btn.svg";
 import { ReactComponent as Cart } from "../../images/cart.svg";
 import { ReactComponent as BurgerMenu } from "../../images/burger-menu.svg";
+import { ReactComponent as Close } from "../../images/close.svg";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      click: false,
+    };
+  }
+
   render() {
     return (
-      <div className="navbar flex-container">
-        <div className="logo flex-child">
-          <h2>Suho Shoes</h2>
-        </div>
+      <div>
+        <div
+          className={`background-overlay ${this.state.click ? "active" : ""}`}
+          onClick={() => this.setState({ click: !this.state.click })}
+        ></div>
 
-        <div className="nav-icons flex-child">
-          <Search width={23} height={23} className="nav-icon-child"/>
-          <Cart width={22} height={24.91} className="nav-icon-child"/>
-          <BurgerMenu width={23} height={17.52} className="nav-icon-child"/>
+        <div className="navbar flex-container">
+          <div className="logo flex-child">
+            <h2>Suho Shoes</h2>
+          </div>
+
+          <div className="nav-icons flex-child">
+            <Search width={23} height={23} className="nav-icon-child" />
+            <Cart width={22} height={24.91} className="nav-icon-child" />
+            <BurgerMenu
+              width={23}
+              height={17.52}
+              className="nav-icon-child"
+              onClick={() => this.setState({ click: !this.state.click })}
+            />
+          </div>
+        </div>
+        <div className={`side-nav ${this.state.click ? "active" : ""}`}>
+          <h2>Suho Shoes</h2>
+          <Close
+            width={12}
+            height={12}
+            className="close-btn"
+            onClick={() => this.setState({ click: !this.state.click })}
+          />
+          <a href="#section">Men</a>
+          <a href="#section">Women</a>
+          <a href="#section">Kids</a>
+          <a href="#section">New Arrivals</a>
+          <a href="#section">Sale</a>
+          <hr />
+          <a href="#section" className="section2">
+            Account
+          </a>
+          <a href="#section" className="section2">
+            Returns &amp; Exchanges
+          </a>
         </div>
       </div>
     );
