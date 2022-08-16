3030ff243e2555a82d7b2ab871492ff42385b67d
Juana Ng <juanang@Juanas-MacBook-Pro.local>
2022-08-16 16:11:04 -0400

    adding routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./homepage/Homepage";
 import "./index.css";
 
@@ -6,7 +7,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Homepage/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/men-shoes" />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
