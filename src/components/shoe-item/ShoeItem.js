f60394b96f6cc7aafb1fa6750b7ddd9481788821
Juana Ng <juanang@Juanas-MacBook-Pro.local>
2022-09-08 16:58:34 -0400

    added review text

--- src/components/shoe-item/ShoeItem.js
+++ src/components/shoe-item/ShoeItem.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { ReactComponent as Star } from "../../images/Star.svg";
 import "./ShoeItem.css";
 
 function ShoeItem() {
@@ -24,18 +25,31 @@ function ShoeItem() {
     findShoeData(shoeId);
   }, [shoeId]);
 
-  console.log(shoeData);
+  // console.log(shoeData);
 
   return (
     <div>
       {shoeData.length && (
-        <div>
+        <div className="shoe-item">
           <img
-            src={shoeData[0].productImage ? require(`../../images/${shoeData[0].productImage}`) : ''}
+            src={
+              shoeData[0].productImage
+                ? require(`../../images/${shoeData[0].productImage}`)
+                : ""
+            }
             alt={shoeData[0].name}
             className="product-img"
           />
-          <h2>{shoeData[0].name}</h2>
+          <h5>Men's</h5>
+          <h4>{shoeData[0].name}</h4>
+          <h4 className="shoe-item-price">{shoeData[0].price}</h4>
+          <div className="reviews">
+            <h5>
+              (<span>4.9</span>
+            </h5>
+            <Star width={15} height={15} className="star" />
+            <h5>{`)`}</h5>
+          </div>
         </div>
       )}
     </div>
