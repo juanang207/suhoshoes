18166aae5de4179bb45001de4e7f58ba78b533a4
Juana Ng <juanang@Juanas-MacBook-Pro.local>
2022-09-08 11:59:52 -0400

    added server

--- src/components/shoe-item/ShoeItem.js
+++ src/components/shoe-item/ShoeItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useParams} from 'react-router-dom';
+import "./ShoeItem.css";
 
 function ShoeItem() {
   
@@ -7,6 +8,7 @@ function ShoeItem() {
 
     return (
       <div>
+        <img src={require("../../images/shoes-img.png")} alt={shoeId} className="product-img"/>
         <h1>Hello</h1>
         <h1>{shoeId}</h1>
       </div>
