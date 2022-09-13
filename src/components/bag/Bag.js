6b3b8bd4f4b44e79998d328730554e6768df0d11
Juana Ng <juanang@Juanas-MacBook-Pro.local>
2022-09-13 10:52:50 -0400

    styled bag/cart page

--- src/components/bag/Bag.js
+++ src/components/bag/Bag.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { ReactComponent as Close } from "../../images/close.svg";
 import { ReactComponent as Arrow } from "../../images/Arrow.svg";
+import Input from "../input/Input";
+import ButtonItem from "../button-item/ButtonItem";
 import "./Bag.css";
 
 class Bag extends Component {
@@ -42,6 +44,42 @@ class Bag extends Component {
 
         <div className="bag-items">
           <BagItem />
+        </div>
+
+        <div className="promo-code">
+          <Input
+            width="323px"
+            paddingLeft="10px"
+            placeholderText="Enter promo code"
+          />
+        </div>
+
+        <div className="order-summary">
+          <h4>Order Summary</h4>
+
+          <div className="order-details">
+            <p>1 Item</p>
+            <p className="right-align">$100.00</p>
+            <p>Shipping</p>
+            <p className="right-align">$5.99</p>
+            <p>Sales Tax</p>
+            <p className="right-align">-</p>
+            <p className="order-total">Total</p>
+            <p className="order-total right-align">$105.99</p>
+          </div>
+        </div>
+
+        <div className="checkout-btns">
+          {/* <div>
+          <input type="image" alt="Paypal Checkout" src={require(`../../images/Paypal.png`)} className="paypal-btn"/>
+          </div> */}
+          <button className="paypal-btn">
+            <img
+              src={require(`../../images/Paypal.png`)}
+              alt={`paypal checkout`}
+            />
+          </button>
+          <ButtonItem text="Checkout" />
         </div>
       </div>
     );
