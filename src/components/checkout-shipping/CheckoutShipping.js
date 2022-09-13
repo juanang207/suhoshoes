6cf03b48f25839b7f6afc92ab21c826121c871d2
Juana Ng <juanang@Juanas-MacBook-Pro.local>
2022-09-13 16:50:27 -0400

    created checkout tabs

--- src/components/checkout-shipping/CheckoutShipping.js
+++ src/components/checkout-shipping/CheckoutShipping.js
@@ -3,9 +3,22 @@ import "./CheckoutShipping.css";
 
 class CheckoutShipping extends Component {
   render() {
+
+    const CheckoutTab = (props) => {
+      return (
+        <div className="checkout-tab">
+          <div className="circle">{props.step}</div>
+          <p>{props.tabName}</p>
+        </div>
+      )
+    } 
+
+
     return (
       <div className="checkout-shipping">
-        <h2>Hi</h2>
+        <CheckoutTab tabName="Shipping" step="1"/>
+        <CheckoutTab tabName="Payment" step="2"/>
+        <CheckoutTab tabName="Review" step="3"/>
       </div>
     );
   }
