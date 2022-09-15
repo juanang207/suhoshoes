8a81b3c96f1440de6aac8674f811b596b0f67f6d
Juana Ng <juanang@Juanas-MacBook-Pro.local>
2022-09-15 14:49:45 -0400

    added billing address fields

--- src/components/checkout-payment/CheckoutPayment.js
+++ src/components/checkout-payment/CheckoutPayment.js
@@ -12,6 +12,7 @@ class CheckoutPayment extends Component {
         { isSelected: true, name: "Credit Card" },
         { isSelected: false, name: "Gift Card" },
       ],
+      billingCheck: false,
     };
   }
 
@@ -58,6 +59,19 @@ class CheckoutPayment extends Component {
       this.setState({ paymentOptions: updatedPaymentOptions });
     };
 
+    const checkPaymentOptionSelected = (name) => {
+      const result = this.state.paymentOptions.find((obj) => obj.name === name);
+      return result.isSelected;
+    };
+
+    const handleBillingChange = (event) => {
+      if (event.target.checked) {
+        this.setState({ billingCheck: true });
+      } else {
+        this.setState({ billingCheck: false });
+      }
+    };
+
     return (
       <div className="checkout-payment">
         <CheckoutTabs page="Payment" completed={["Shipping"]} />
@@ -76,30 +90,55 @@ class CheckoutPayment extends Component {
             ))}
           </div>
 
-          <div className="creditcard-form">
-            <FormField labelName="Cardholder Full Name" width="323px" />
-            <FormField
-              labelName="Card Number"
-              width="323px"
-              boxType="credit-card"
-            />
-            <div className="creditcard-details">
-              <FormField labelName="Expiration Date" width="150.5px" />
+          {checkPaymentOptionSelected("Credit Card") && (
+            <div className="creditcard-form">
+              <FormField labelName="Cardholder Full Name" width="323px" />
               <FormField
-                labelName="Security Code"
-                width="150.5px"
+                labelName="Card Number"
+                width="323px"
                 boxType="credit-card"
               />
-            </div>
+              <div className="creditcard-details">
+                <FormField labelName="Expiration Date" width="150.5px" />
+                <FormField
+                  labelName="Security Code"
+                  width="150.5px"
+                  boxType="credit-card"
+                />
+              </div>
 
-            <div className="billing-address">
-              <input type="checkbox" id="check-input" />
+              <div className="billing-address">
+                <div className="billing-address-checkbox">
+                  <input
+                    type="checkbox"
+                    id="check-input"
+                    onChange={handleBillingChange}
+                  />
+                  <label htmlFor="check-input">
+                    My billing address is the same as my shipping address.
+                  </label>
+                </div>
 
-              <label for="check-input">
-                My billing address is the same as my shipping address.
-              </label>
+                {!this.state.billingCheck && (
+                  <>
+                    <h3>Billing Address</h3>
+                    <FormField labelName="Address" width="323px" />
+                    <div className="address-details">
+                      <FormField labelName="City" width="132px" />
+                      <FormField labelName="State" width="47px" />
+                      <FormField labelName="Zipcode" width="99px" />
+                    </div>
+                  </>
+                )}
+              </div>
             </div>
-          </div>
+          )}
+
+          {checkPaymentOptionSelected("Gift Card") && (
+            <div className="giftcard-form">
+              <FormField labelName="Gift Card Number" width="323px" />
+            </div>
+          )}
 
           <div className="review-order-btn">
             <ButtonItem text="Review Order" />
